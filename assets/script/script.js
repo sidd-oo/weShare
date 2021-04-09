e5157658f45c57654aff1f8d4d94eecab1b7ba31
sidd_oo <sahoosiddharth3@gmail.com>
2021-04-09 07:55:34 +0530

    upload functionality added

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,9 @@
 const dropZone = document.querySelector(".drop-zone")
 const fileInput = document.querySelector("#fileInput")
 const browseBtn = document.querySelector('.browseBtn')
+const host =  "https://innshare.herokuapp.com/"
+const uploadURL = host + "api/files"
+// const uploadURL = host + "api/files"
 
 dropZone.addEventListener("dragover",(e)=>{
     e.preventDefault();
@@ -20,9 +23,29 @@ dropZone.addEventListener("drop", (e)=>{
     console.log(files);
     if(files.length){
         fileInput.files = files;
+        uploadFile()
     }
 })
 
 browseBtn.addEventListener('click', (e)=>{
     fileInput.click()
 })
+
+fileInput.addEventListener("change",()=>{
+    uploadFile()
+})
+
+const uploadFile = ()=>{
+    const file = fileInput.files[0]
+    const formData = new FormData()
+    formData.append("myfile",file)
+
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = ()=>{
+        if(xhr.readyState === XMLHttpRequest.DONE){
+            console.log(xhr.response);
+        }
+    }
+    xhr.open("POST",uploadURL)
+    xhr.send(formData)
+}
