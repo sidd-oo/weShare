f57770e89fab622ff2362c2b77aec8e8cc109293
sidd-oo <sahoosiddharth3@gmail.com>
2022-03-14 21:44:06 +0530

    feat: designed progress bar for the file upload update

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,51 +1,75 @@
-const dropZone = document.querySelector(".drop-zone")
-const fileInput = document.querySelector("#fileInput")
-const browseBtn = document.querySelector('.browseBtn')
-const host =  "https://innshare.herokuapp.com/"
-const uploadURL = host + "api/files"
+const dropZone = document.querySelector(".drop-zone");
+const fileInput = document.querySelector("#fileInput");
+const browseBtn = document.querySelector(".browseBtn");
+const progressContainer = document.querySelector(".progress-container");
+const bgProgress = document.querySelector(".bg-progress");
+const progressBar = document.querySelector(".progress-bar");
+const percentDiv = document.querySelector("#percent");
+
+const host = "https://innshare.herokuapp.com/";
+const uploadURL = host + "api/files";
 // const uploadURL = host + "api/files"
 
-dropZone.addEventListener("dragover",(e)=>{
-    e.preventDefault();
-    if(!dropZone.classList.contains("dragged")){
-        dropZone.classList.add("dragged")
+dropZone.addEventListener("dragover", (e) => {
+  e.preventDefault();
+  if (!dropZone.classList.contains("dragged")) {
+    dropZone.classList.add("dragged");
+  }
+});
+
+dropZone.addEventListener("dragleave", () => {
+  dropZone.classList.remove("dragged");
+});
+
+dropZone.addEventListener("drop", (e) => {
+  e.preventDefault();
+  dropZone.classList.remove("dragged");
+  const files = e.dataTransfer.files;
+  console.log(files);
+  if (files.length) {
+    fileInput.files = files;
+    uploadFile();
+  }
+});
+
+browseBtn.addEventListener("click", (e) => {
+  fileInput.click();
+});
+
+fileInput.addEventListener("change", () => {
+  uploadFile();
+});
+
+const uploadFile = () => {
+  const file = fileInput.files[0];
+  const formData = new FormData();
+
+  progressContainer.style.display = "block";
+  formData.append("myfile", file);
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === XMLHttpRequest.DONE) {
+      console.log(xhr.response);
+      showLink(JSON.parse(xhr.response));
     }
-})
+  };
 
-dropZone.addEventListener("dragleave", ()=>{
-    dropZone.classList.remove('dragged')
-})
+  xhr.upload.onprogress = updateProgress;
 
-dropZone.addEventListener("drop", (e)=>{
-    e.preventDefault();
-    dropZone.classList.remove('dragged')
-    const files = e.dataTransfer.files;
-    console.log(files);
-    if(files.length){
-        fileInput.files = files;
-        uploadFile()
-    }
-})
+  xhr.open("POST", uploadURL);
+  xhr.send(formData);
+};
 
-browseBtn.addEventListener('click', (e)=>{
-    fileInput.click()
-})
+const updateProgress = (e) => {
+  const percentageProgress = Math.round((e.loaded / e.total) * 100);
+  console.log(percentageProgress);
+  bgProgress.style.width = `${percentageProgress}%`;
+  percentDiv.innerText = percentageProgress;
+  progressBar.style.transform = `scaleX(${percentageProgress}/100)`;
+};
 
-fileInput.addEventListener("change",()=>{
-    uploadFile()
-})
-
-const uploadFile = ()=>{
-    const file = fileInput.files[0]
-    const formData = new FormData()
-    formData.append("myfile",file)
-
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = ()=>{
-        if(xhr.readyState === XMLHttpRequest.DONE){
-            console.log(xhr.response);
-        }
-    }
-    xhr.open("POST",uploadURL)
-    xhr.send(formData)
-}
+const showLink = ({ file }) => {
+  console.log(file);
+  progressContainer.style.display = "none";
+};
