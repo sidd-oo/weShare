1df4e665e08b8decd3a4000f0b06cf5f681dd381
Siddharth Sahoo <sahoosiddharth3@gmail.com>
2023-05-27 07:33:37 +0530

    fix: host name change

--- assets/script/script.js
+++ assets/script/script.js
@@ -14,7 +14,7 @@ const copyBtn = document.querySelector("#copyBtn");
 const emailForm = document.querySelector("#emailForm");
 const toast = document.querySelector(".toast");
 
-const host = `https://we-share-file-sharing.herokuapp.com/`
+const host = `https://we-share-backend-fwp7.onrender.com`
 const uploadURL = `${host}api/files`;
 const emailURL = `${host}api/files/send`;
 const maxAllowedSize = 100 * 1024 * 1024; // 100mb
